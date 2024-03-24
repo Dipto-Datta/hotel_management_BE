878fd73c795a5e86a44604031c8ffab8eb04a047
Dipto-Datta <diptodattag@gmail.com>
2024-03-24 14:16:50 +0600

    update version 1

---
+++ core/urls.py
@@ -0,0 +1,57 @@
+from django.contrib import admin
+from django.urls import path, re_path, include
+from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from rest_framework.routers import DefaultRouter
+from . import views
+from rest_framework_simplejwt import views as jwt_views
+from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
+router = DefaultRouter()
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework_simplejwt.views import TokenVerifyView, TokenRefreshView
+
+# create a router and register our viewsets with it
+# router.register(r'v1/subject-category-list', views.SubjectCategoryViewSet)
+
+# router.register(r"v1/user", user_list_view)
+# router.register(r"v1/group", group_list_view)
+# router.register(r"v1/permission", permission_list_view)
+urlpatterns = [
+    path("", include(router.urls)),
+    # path("login/", views.login, name="user_login"),
+    # path("registration/", views.registration, name="user_registration"),
+    # # create superuser
+    # path("superuser/", views.create_super_user, name="create_super_user"),
+    # # this endpoint returns-
+    # #   status 200 ok and returns "{}", if token is valid
+    # #   else throws 401 with error message
+    # #  TokenVerifyView.as_view()
+    # # path('token/verify/',  views.token_validation, name='token_verify'),
+    # path("token/", jwt_views.TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    # path("token/refresh/", jwt_views.TokenRefreshView.as_view(), name="token_refresh"),
+    # # Todo : CSRF issue resolve
+    # # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path("password/change/", views.change_password, name="change_password"),
+    # path("password/forget/", views.forget_password, name="forget_password"),
+    # path(
+    #     "password/forget-confirm/",
+    #     views.forget_password_confirm,
+    #     name="forget_password_confirm",
+    # ),
+    # path("logout/", views.logout, name="logout"),
+    # path(
+    #     "add-permission-to-group/",
+    #     views.add_permission_to_group,
+    #     name="add_permission_to_group",
+    # ),
+    # path("add-user-to-group/", views.add_user_to_group, name="add_user_to_group"),
+    # path(
+    #     "remove-user-from-group/",
+    #     views.remove_user_from_group,
+    #     name="remove_user_from_group",
+    # ),
+    #
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- hm_project/urls.py
+++ hm_project/urls.py
@@ -1,7 +1,8 @@
-"""hm_project URL Configuration
+"""
+URL configuration for main project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,25 +14,35 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path, include, re_path
-from core.views import IndexTemplateView
-from django.conf.urls.static import static
-from django.conf import settings
+from django.urls import path, include
 
+# swagger config
+from rest_framework import permissions
 
+# from drf_yasg.views import get_schema_view
+# from drf_yasg import openapi
+
+# swagger views settings
+
+# schema_view = get_schema_view(
+#    openapi.Info(
+#     title="Swagger with django API",
+#     default_version='v1',
+#     description="powered by spaceyatech and Tamarcom Technology",
+#     terms_of_service="https://www.ourapp.com/policies/terms/",
+#     contact=openapi.Contact(email="contact@expense.local"),
+#     license=openapi.License(name="Test License"),),
+#     public=True,
+#     permission_classes=[permissions.AllowAny],
+# )
 urlpatterns = [
-    path('auth/', include('authentication.urls')),
-    # path('', IndexTemplateView.as_view(), name="home"),
-    path('admin/', admin.site.urls),
-    path('api/', include('hm_backend.urls')),
-    # re_path(r"^.*$", IndexTemplateView.as_view(), name="home")
-    re_path(r"^(?!media).*$", IndexTemplateView.as_view(), name='home'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
-
-handler404 = "helpers.views.handle_not_found"
-handler500 = "helpers.views.handle_server_error"
+    path("admin/", admin.site.urls),
+    # for swagger ui
+    # path ('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # auth urls --only for authentication and authorization purpose
+    path("auth/", include("core.urls")),
+    # app urls -- /api/appname/ -- for general apps
+    # path("api/guide/", include("guide.urls")),
+]
